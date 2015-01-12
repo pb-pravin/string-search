cf0f8b558eb8a15339e424fb801b95a15df73edb
AndrewRademacher <andrewrademacher@gmail.com>
2015-01-11 20:14:54 -0600

    Added criterion.

--- src/Main.hs
+++ src/Main.hs
@@ -1,4 +1,18 @@
 module Main where
 
+import           Criterion
+import           Criterion.Main
+
+import           Search.Naive.String
+
 main :: IO ()
-main = print "hello"
+main = defaultMain
+        [ env (readFile "texts/tomsawyer.txt") $ \f ->
+            bgroup "Tom Sayer (412k)"
+                [ bench "String" $ whnf (\p -> find f p) "Hello"
+                ]
+        , env (readFile "texts/huckfinn.txt") $ \f ->
+            bgroup "Huck Finn (596k)"
+                [ bench "String" $ whnf (\p -> find f p) "Hello"
+                ]
+        ]
